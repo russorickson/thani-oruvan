bb986b7347b369de6d6ec3870818880e8635e67d
russorickson <russorickson831@gmail.com>
2017-03-27 15:42:18 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Sample
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		System.out.println("hi");
+	
+}
